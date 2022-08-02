1c7e7c194ddb882a614bfd65ac2fdd879e84b324
Mujeeb Nazir <95225496+mujeebnazir@users.noreply.github.com>
2022-08-02 17:18:01 +0530

    Create 05-ProductOf2Integers.c

---
+++ 05-ProductOf2Integers.c
@@ -0,0 +1,10 @@
+//program to multiply to two integers.
+int main()
+{
+	int a,b,c;
+	printf("Enter two integers you want to multiply: ");
+	scanf("%d %d",&a,&b);
+	c= a*b;
+	printf("%d multiplyed by %d is %d",a,b,c);
+	return 0;
+}
