83554779a27e68cbfdb81ddff5031b6013b5700f
Mujeeb Nazir <95225496+mujeebnazir@users.noreply.github.com>
2022-09-21 21:54:36 +0530

    Create 32-SunOfNaturalNumViaRecursion.c

---
+++ 32-SunOfNaturalNumViaRecursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int recAdd(int);
+int main(){
+	int n,a;
+	printf("Enter the number: \n");
+	scanf("%d",&n);
+
+	a = recAdd(n);
+	printf("Sum of 1st %d natural numbers is = %d",n,a);
+}
+
+int recAdd(int n){
+	if(n==1){
+		return 1;
+	}
+	else{
+		return n+recFact(n-1);
+	}
+}
